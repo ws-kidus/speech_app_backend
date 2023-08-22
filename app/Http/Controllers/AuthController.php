5ece90ed12c69b414bb6a0ed9ca84353e42dd3b9
kidus <kidus.ws@gmail.com>
2023-08-22 11:03:38 +0300

    social media save image

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,8 @@ class AuthController extends Controller
 {
     // signUp
     public function signUp(Request $request)
-    {$type = 1;
+    {
+        $type = 1;
 
         $request->validate([
             'name' => 'required',
@@ -105,10 +106,15 @@ class AuthController extends Controller
         $name = $request['name'];
         $email = $request['email'];
         $password = $request['password'];
-        $photoUrl = $request['photoUrl'];
         $type = $request['type'];
 
         $user = User::where('email', $email)->first();
+
+        $photoUrl = null;
+        if ($request['photoUrl']) {
+            $imageName = $this->storeSocialImage($name, $request['photoUrl']);
+            $photoUrl = '/uploads/' . $imageName;
+        }
 
         if (!$user) {
             $user = User::create([
@@ -131,6 +137,40 @@ class AuthController extends Controller
         ];
 
         return Response($response, 200);
+    }
+
+    public function storeSocialImage($name, $url)
+    {
+        // Initialize the cURL session
+        $ch = curl_init($url);
+
+        // Initialize directory name where
+        // file will be save
+        $dir = public_path() . '/uploads' . '/';
+
+        // Use basename() function to return
+        // the base name of file
+        $file_name = time().$name;
+
+        // Save file into file location
+        $save_file_loc = $dir . $file_name;
+
+        // Open file
+        $fp = fopen($save_file_loc, 'wb');
+
+        // It set an option for a cURL transfer
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        // Perform a cURL session
+        curl_exec($ch);
+
+        // Closes a cURL session and frees all resources
+        curl_close($ch);
+
+        // Close file
+        fclose($fp);
+        return $file_name;
     }
 
 }
